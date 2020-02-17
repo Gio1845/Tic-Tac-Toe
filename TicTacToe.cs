9cca0076083d405d751c63373fad07e64ded3a59
Gio1845 <victor-reyesr@hotmail.com>
2020-02-17 08:33:08 -0700

    agregar valores en matrix

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -7,9 +7,28 @@ namespace Tic_Tac_Toe
         static int MATRIX_SIZE = 3;
 
         //static char[,] matrix = new char[3, 3] {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-        static char[,] matrix = new char[3, 3] {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+        /// <summary>
+        /// matrix array [y,x]
+        /// </summary>
+        /// <value>Empty matrix</value>
+        static char[,] matrix = new char[3, 3] {
+            {' ', ' ', ' '}, 
+            {' ', ' ', ' '}, 
+            {' ', ' ', ' '}
+            
+            };
+            /// <summary>
+            /// prints the matrix
+            /// </summary>
 
         static void PrintMatrix (){
+            /// <summary>
+            /// adds a value to the matrix in the especified position.
+            /// </summary>
+            /// <param name= "value"> value to add</param>
+            /// <param name= "y"> y position</param>
+            /// <param name= "x"> x position</param>
               for(int y = 0; y < MATRIX_SIZE; y++){
                 string line = "";
                 for(int x = 0; x < MATRIX_SIZE; x++){
@@ -23,11 +42,16 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("------");
             }
         }
+        static void AddValue(char value, int y, int x) {
+            matrix[y,x] = value;
+
+        }
         static void Main(string[] args)
         {
           
             PrintMatrix();
-            
+            AddValue ('x', 0, 0);
+            PrintMatrix();
             }
         }
     }
