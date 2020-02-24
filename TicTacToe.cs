9e5f30fe54f910866f3a441287713e1b1d29662c
Gio1845 <victor-reyesr@hotmail.com>
2020-02-24 16:15:34 -0700

    fix/inputrequest/update

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -4,6 +4,9 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
+        /// <summary>
+        /// Matrix length
+        /// </summary>
         static int MATRIX_SIZE = 3;
 
         //static char[,] matrix = new char[3, 3] {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
@@ -13,27 +16,30 @@ namespace Tic_Tac_Toe
         /// </summary>
         /// <value>Empty matrix</value>
         static char[,] matrix = new char[3, 3] {
-            {' ', ' ', ' '}, 
-            {' ', ' ', ' '}, 
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
             {' ', ' ', ' '}
-            
-            };
-            /// <summary>
-            /// prints the matrix
-            /// </summary>
 
-        static void PrintMatrix (){
+            };
+        /// <summary>
+        /// prints the matrix
+        /// </summary>
+
+        static void PrintMatrix()
+        {
             /// <summary>
             /// adds a value to the matrix in the especified position.
             /// </summary>
             /// <param name= "value"> value to add</param>
             /// <param name= "y"> y position</param>
             /// <param name= "x"> x position</param>
-              for(int y = 0; y < MATRIX_SIZE; y++){
+            for (int y = 0; y < MATRIX_SIZE; y++)
+            {
                 string line = "";
-                for(int x = 0; x < MATRIX_SIZE; x++){
+                for (int x = 0; x < MATRIX_SIZE; x++)
+                {
                     //interpolate string
-                   // Console.WriteLine($"[y, x] = {y}, {x}");
+                    // Console.WriteLine($"[y, x] = {y}, {x}");
                     line += matrix[y, x] + "|";
                 }
                 line = line.Substring(0, line.Length - 1);
@@ -42,70 +48,206 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("------");
             }
         }
-        static void AddValue(char value, int y, int x) {
-            matrix[y,x] = value;
+        static void AddValue(char value, int y, int x)
+        {
+            matrix[y, x] = value;
 
         }
 
-        static void InputRequest(){
-            Console.WriteLine("Escribe las cordenadas de la fomra y,x donde quieres hacer tu movimiento y presiona enter");
-            string userInoutCoordinates = Console.ReadLine();
+        static void InputRequest()
+        {
+             bool validPositionSelected = false;
 
-            //quitar espacio
-            userInoutCoordinates = userInoutCoordinates.Replace(" ", " ");
+            int y = 0;
+            int x = 0;
 
-            //separar en un arreglo de valores con ","
-            string[] coordinates = userInoutCoordinates.Split(",");
+            while(!validPositionSelected){
+                Console.WriteLine("Escribe las cordenadas de la fomra y,x donde quieres hacer tu movimiento y presiona enter");
+                string userInoutCoordinates = Console.ReadLine();
+
+                //quitar espacio
+                userInoutCoordinates = userInoutCoordinates.Replace(" ", " ");
+
+                //separar en un arreglo de valores con ","
+                string[] coordinates = userInoutCoordinates.Split(",");
 
 
-            //convertir en cordenadas tipo entero
-            int y = Convert.ToInt32(coordinates[0]);
-            int x = Convert.ToInt32(coordinates[1]);
+                //convertir en cordenadas tipo entero
+                y = Convert.ToInt32(coordinates[0]);
+                x = Convert.ToInt32(coordinates[1]);
+                bool isValueDefined = IsValueInMatrix(y, x);
+
+                validPositionSelected = !isValueDefined;
+
+                if(!validPositionSelected){
+                    Console.WriteLine("esa pocision ya esta ocupada");
+                }
+            }
 
             AddValue('X', y, x);
         }
 
-        static bool IsValueInMatrix(int y, int x){
-            bool isEmpty = matrix[y,x] == ' ';
+        static bool IsValueInMatrix(int y, int x)
+        {
+            bool isEmpty = matrix[y, x] == ' ';
 
             return !isEmpty;
         }
-        static void AiRequest(){
+        static void AiRequest()
+        {
             Random r = new Random();
 
-          
+
 
             bool validPositionSelected = false;
 
             int y = 0;
             int x = 0;
 
-           while(!validPositionSelected){
-                 y = (int) Math.Floor(r.NextDouble() * 3);
-                 x = Convert.ToInt32(Math.Floor(r.NextDouble() * 3));
-                bool isValueDefined = IsValueInMatrix(y,x);
+            while (!validPositionSelected)
+            {
+
+                y = (int)Math.Floor(r.NextDouble() * 3);
+                x = Convert.ToInt32(Math.Floor(r.NextDouble() * 3));
+                bool isValueDefined = IsValueInMatrix(y, x);
 
                 validPositionSelected = !isValueDefined;
-           }
+            }
             //add to matrix
             AddValue('O', y, x);
         }
+        static bool CheckTheeLines()
+        {
+            char value = ' ';
+            bool sameValue = true;
+            //filas
+            /* int y = 0;
+            char value = ' ';
+            bool sameValue = true;
+            for(int x = 0; x < 3; x++){
+                if(x == 0){
+                value = matrix[y,x];
+                }else
+                {
+                    sameValue = sameValue && (value == matrix[y, x]);
+                }
+            } */
+            for (int y = 0; y < 3; y++)
+            {
+                 value = ' ';
+                 sameValue = true;
+                for (int x = 0; x < 3; x++)
+                {
+                    if (x == 0)
+                    {
+                        value = matrix[y, x];
+                    }
+                    else
+                    {
+                        sameValue = sameValue && (value == matrix[y, x]);
+                    }
+                }
+                if (sameValue && value != ' ')
+                {
+                    return true;
+                }
 
+            }
+            //columnas
+            for (int x = 0; x < 3; x++)
+            {
+                 value = ' ';
+                 sameValue = true;
+                for (int y = 0; y < 3; y++)
+                {
+                    if (y == 0)
+                    {
+                        value = matrix[y, x];
+                    }
+                    else
+                    {
+                        sameValue = sameValue && (value == matrix[y, x]);
+                    }
+                }
+                if (sameValue && value != ' '){
+                    return true;
+                }
+            }
+
+            //diagonales
+
+            for (int i = 0; i < 3; i++){
+                if( i == 0){
+                    value = matrix[i, i];
+                }else{
+                    sameValue = sameValue && (value == matrix[i, i]);
+                }
+
+                if(sameValue  && value != ' ' && i == 2){
+                    return true;
+                }
+
+            }
+
+            value = ' ';
+                   sameValue = true;
+                 for (int y = 0; y < 3; y++) {
+                   int x = 2 - y;
+
+                  if(y == 0) {
+                    value = matrix[y, y];
+                  }else {
+                    sameValue = sameValue && (value == matrix[y, y]);
+                  }
+
+                  if (sameValue && value != ' ' && y == 2) {
+                    return true;
+                  }
+                 }
+            return false;
+        }
         static void Main(string[] args)
         {
-          
-            PrintMatrix();
-            InputRequest();
-            AiRequest();
-            PrintMatrix();
+
+            /*  PrintMatrix();
+             InputRequest();
+             AiRequest();
+             PrintMatrix(); */
+
+             
 
 
             bool gameEnded = false;
             int turns = 0;
+            while (!gameEnded)
+            {
+                InputRequest();
+                turns++;
+                //check if user won
+                gameEnded = CheckTheeLines();
+                //end after 9 turns
+                if (turns >= 9)
+                {
+                    gameEnded = true;
+                }
+                if (!gameEnded)
+                {
+                    AiRequest();
+                    turns++;
+                    //Check if Ai won
+                    gameEnded = CheckTheeLines();
+                }
+                Console.WriteLine();
+                PrintMatrix();
+
 
             }
-            
-        }
-    }
+            PrintMatrix();
+            Console.WriteLine("Game Over");
 
-           
+        }
+
+
+    }
+}
+
